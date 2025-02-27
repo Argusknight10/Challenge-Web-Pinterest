ae14706e9eb1da7010e94e7355783509325cd047
argus <arguspermono19@gmail.com>
2025-02-27 09:10:48 +0700

    delete view to avoid conflict

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,3 @@ use App\Http\Controllers\PhotoController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::get('storage-link', function () {
-    Artisan::call('storage:link');
-});
-
-Route::resource('articles', PhotoController::class)->scoped(['article' => 'slug']);
